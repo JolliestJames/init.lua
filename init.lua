e7208beaed2f0bc5a646f4eabbdd37cd6dbc1a37
jame <cloudismydog@gmail.com>
2023-12-09 11:51:20 -0800

    bye bye packer, hi lazy

--- init.lua
+++ init.lua
@@ -1,1 +1,73 @@
 require("james")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+  {'mrjones2014/smart-splits.nvim', lazy = false, priority = 1000},
+  {'williamboman/mason.nvim'},
+  {'williamboman/mason-lspconfig.nvim'},
+  {'neovim/nvim-lspconfig'},
+  {'hrsh7th/cmp-nvim-lsp'},
+  {'hrsh7th/nvim-cmp'},
+  {'L3MON4D3/LuaSnip'},
+  {'nvim-treesitter/nvim-treesitter'},
+  {'nvim-treesitter/playground'},
+  {'theprimeagen/harpoon'},
+  {'mbbill/undotree'},
+  {'tpope/vim-fugitive'},
+  {'maxmx03/dracula.nvim',
+   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+   priority = 1000, -- make sure to load this before all the other start plugins
+   name = 'dracula',
+   config = function ()
+   local dracula = require 'dracula'
+
+       dracula.setup()
+
+       vim.cmd.colorscheme 'dracula'
+  end},
+  {'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  }
+})
+
+require('smart-splits').setup({
+  -- add any options here
+  multiplexer_integration = 'WezTerm'
+})
+
+--local mux = require('smart-splits.mux').get()
+--print(mux.type)
+
+-- recommended mappings
+-- resizing splits
+-- these keymaps will also accept a range,
+-- for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
+vim.keymap.set('n', '<A-h>', require('smart-splits').resize_left)
+vim.keymap.set('n', '<A-j>', require('smart-splits').resize_down)
+vim.keymap.set('n', '<A-k>', require('smart-splits').resize_up)
+vim.keymap.set('n', '<A-l>', require('smart-splits').resize_right)
+-- moving between splits
+vim.keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left)
+vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
+vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
+vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right)
+-- swapping buffers between windows
+vim.keymap.set('n', '<leader><leader>h', require('smart-splits').swap_buf_left)
+vim.keymap.set('n', '<leader><leader>j', require('smart-splits').swap_buf_down)
+vim.keymap.set('n', '<leader><leader>k', require('smart-splits').swap_buf_up)
+vim.keymap.set('n', '<leader><leader>l', require('smart-splits').swap_buf_right)
+
