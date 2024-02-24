8c322f80d603ee91eba2762195ed6103a21512d3
JolliestJames <cloudismydog@gmail.com>
2024-02-24 12:42:05 -0800

    make it easy to comment out lsps that may not be needed between machines

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -13,7 +13,14 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here
   -- with the ones you want to install
-  ensure_installed = {'tsserver', 'rust_analyzer', 'elixirls', 'gopls', 'ruby_ls', 'zls'},
+  ensure_installed = {
+      'tsserver',
+      'rust_analyzer',
+      'elixirls',
+      'gopls',
+      'ruby_ls',
+      'zls'
+  },
   handlers = {
     lsp.default_setup,
   },
