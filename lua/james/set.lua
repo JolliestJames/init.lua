afa3bb0b61d1276676de7766945943ee300ff104
JolliestJames <cloudismydog@gmail.com>
2024-02-24 12:39:22 -0800

    update set.lua

--- lua/james/set.lua
+++ lua/james/set.lua
@@ -1,7 +1,6 @@
-vim.opt.guicursor = ""
-
 vim.opt.nu = true
-vim.opt.relativenumber = true
+--vim.opt.relativenumber = true
+vim.opt.number = false
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -14,7 +13,9 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+
+vim.opt.undodir = os.getenv("HOME") or os.getenv("HOMEPATH") .. "/.vim/undodir"
+
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
