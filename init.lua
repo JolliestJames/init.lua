1a07714d2787d51a1638cc74f154dfcf5ea01be8
JolliestJames <cloudismydog@gmail.com>
2024-02-27 12:31:08 -0800

    try all the themes

--- init.lua
+++ init.lua
@@ -29,6 +29,34 @@ require("lazy").setup({
   {'tpope/vim-fugitive'},
   {'rose-pine/neovim', name = 'rose-pine'},
   { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "neanias/everforest-nvim",
+    version = false,
+    lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    -- Optional; default configuration will be used if setup isn't called.
+    config = function()
+      require("everforest").setup({
+        -- Your config here
+      })
+    end,
+  },
+  { "EdenEast/nightfox.nvim" },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "Tsuzat/NeoSolarized.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      vim.cmd [[ colorscheme NeoSolarized ]]
+    end
+  },
   {'maxmx03/dracula.nvim',
    lazy = false, -- make sure we load this during startup if it is your main colorscheme
    priority = 1000, -- make sure to load this before all the other start plugins
